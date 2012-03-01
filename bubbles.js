3ec29e55a1e5809adb65804ee7fb8043c4f85768
Pantelis Kalogiros <pkalogiros@live.com>
2012-03-01 11:20:48 +0200

    Update bubbles.js

--- bubbles.js
+++ bubbles.js
@@ -7,24 +7,7 @@
 	//Change Log: 
 		* Added Audio Descriptionss
 	____________________________________________________
-	Copyright (C) 2011,  Pantelis Kalogiros
-
-	Permission is hereby granted, free of charge, to any person obtaining a copy
-	of this software and associated documentation files (the "Software"), to deal
-	in the Software without restriction, including without limitation the rights
-	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-	copies of the Software, and to permit persons to whom the Software is
-	furnished to do so, subject to the following conditions:
-
-	The above copyright notice and this permission notice shall be included in
-	all copies or substantial portions of the Software.
-
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	Author	:  Pantelis Kalogiros
 
 	You should have received a copy of the MIT License
 	along with this program.  If not, see <http://www.opensource.org/licenses/mit-license.php>
@@ -630,7 +613,7 @@ function videoBubbles() {
 					c = 0;
 					
 					while( fileLines[ i + ++c ].replace( /^\s+|\s+$/g, "" ) !== "" )
-						ret[ j ][ "text" ] += fileLines[ i + c ].replace( /\n\r|\r\n|\n|\r/g, "<br />" ); //str_replace("'", '"',  ); in php
+						ret[ j ][ "text" ] += fileLines[ i + c ].replace( /\n\r|\r\n|\n|\r/g, "<br />" );
 				}
 			}
 				
